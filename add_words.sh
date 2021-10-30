a5fed3dfd16c51fc4005dc6ec1f63a0eab52f329
Cal Page <page.cal@gmail.com>
2021-10-30 01:38:12 -0400

    add words from Oct 29, 2021

--- add_words.sh
+++ add_words.sh
@@ -8,3 +8,7 @@ echo -e "*hatha\n#" | aspell -a
 echo -e "*metalhead\n#" | aspell -a
 # Words from Oct 28, 2021
 echo -e "*movably\n#" | aspell -a
+# Words from Oct 29, 2021
+echo -e "*itty\n#" | aspell -a
+echo -e "*grittily\n#" | aspell -a
+
